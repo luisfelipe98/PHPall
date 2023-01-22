2cf7c5ce5d33d84b15bf794301d9dd8222055f0e
luisfelipe98 <luisfelipemm98@gmail.com>
2023-01-22 17:24:40 -0300

    Modificando o arquivo variavel.php

--- variavel.php
+++ variavel.php
@@ -8,7 +8,31 @@
 $x = "teste"; // variável $x com o valor teste
 $$x = 5; // variável teste com valor 5
 
-echo $teste; // A variável $$x virou $teste por causa da atribuição do valor de $x ao nome da varável 
+echo $teste . "<br>"; // A variável $$x virou $teste por causa da atribuição do valor de $x ao nome da varável 
+
+// Variável por referência
+// É uma variável que referência o MESMO PONTO DA MEMÓRIA DA OUTRA
+// Utilizamos o símbolo =&
+// Qualquer mudança nas variáveis, muda o resultado das duas
+
+$n1 = 7.5;
+$n2 =& $n1;
+
+echo $n1 . "<br>";
+echo $n2 . "<br>";
+
+// Se mudamos a variável referenciada ($n2), ambas irão mudar
+
+$n2 = 8.7;
+
+echo $n1 . "<br>";
+echo $n2 . "<br>";
+
+// Se mudamos a variável de referência ($n1), ambas irão mudar
+
+$n1 = 17.5;
+
+echo $n1 . "<br>";
+echo $n2 . "<br>";
 
 ?>
-
