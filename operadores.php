c5f75b616689ac30d8fa796396c992be7da818ec
luisfelipe98 <luisfelipemm98@gmail.com>
2023-01-25 14:25:32 -0300

    Adicionando o arquivo operadores.php

---
+++ operadores.php
@@ -0,0 +1,15 @@
+<?php
+
+// Operadores de auto incremento e decremento
+// Serve para adicionar 1 ou decrementar 1
+// Muito utilizados em estruturas de repetição
+
+$incre = 0;
+$decre = 0;
+
+// Utilização dos operadores
+$incre++;
+$decre--;
+
+echo $incre . " " . $decre;
+?>
