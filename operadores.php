67a7366f8b146412343ccbd2945f28ff458f7bca
luisfelipe98 <luisfelipemm98@gmail.com>
2023-01-25 14:41:13 -0300

    Modificando o arquivo operadores.php

--- operadores.php
+++ operadores.php
@@ -11,5 +11,29 @@ $decre = 0;
 $incre++;
 $decre--;
 
-echo $incre . " " . $decre;
+echo $incre . " " . $decre . "<br>";
+
+/* Operadores de comparação
+    Comparam resultados para saber se são verdadeiro ou falso (Retornam um booleano)
+    Operador de igualdade: ==
+    Operador de idêntico a: === (Compara não só o valor, mas o tipo de dado também)
+    Operador de diferença: !=
+    Operador de não idêntico a: !==
+    Operador de maior e maior ou igual a: > e >=
+    Operador de menor e menor ou igual a: < e <= 
+*/
+
+// Exemplo de operador de igualdade e diferença
+$n1 = 10;
+$n2 = 15;
+
+// Exemplo de igualdade
+if ($n1 == 10) {
+    echo "Os números são iguais <br>";
+}
+
+// Exemplo de diferença
+if ($n2 != 10) {
+    echo "Os números são diferentes <br>";
+}
 ?>
