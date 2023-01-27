996578ad228bbbd5d1817762c5d31dc4b2de511c
luisfelipe98 <luisfelipemm98@gmail.com>
2023-01-27 19:30:43 -0300

    Modificando o arquivo if.php

--- Dicas/if.php
+++ Dicas/if.php
@@ -51,4 +51,39 @@ if ($a % $b != $c) {
 } else {
     echo "Deu errado! Entrou dentro do else de variáveis <br>";
 }
+
+// If Aninhado
+// É um if dentro de outro if
+// Será avaliado do jeito que estiverem dispostas, de fora para dentro do bloco
+// É pouco utilizado
+
+// Exemplos
+if (60 % 3 == 0) {
+    if (40 > 4) {
+        echo "Deu certo! Entrou dentro do segundo if <br>"; 
+    }
+}
+
+if (40 != "João") {
+    if (55 >= 101) {
+        echo "Deu certo! Entrou dentro do segundo if <br>"; 
+    } else {
+        echo "Deu errado! Entrou dentro do else do segundo if <br>"; 
+    }
+}
+
+// Usando variáveis
+$num = 77;
+$nome = "Isabel";
+
+if ($num === $nome) {
+    if (47 % 7 == 5) {
+        echo "Deu certo! Entrou dentro do segundo if <br>"; 
+    } else {
+        echo "Deu errado! Entrou dentro do else do segundo if <br>"; 
+    }
+} else {
+   echo "Deu errado! Entrou dentro do else do primeiro if <br>";  
+}
+
 ?>
