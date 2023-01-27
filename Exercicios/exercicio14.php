82cc2d7364cc1bc6518c5abf8564f3bb48eaf6bc
luisfelipe98 <luisfelipemm98@gmail.com>
2023-01-27 19:08:32 -0300

    Adicionando exercicio14.php

---
+++ Exercicios/exercicio14.php
@@ -0,0 +1,33 @@
+<?php
+
+// Faça uma verificação pra saber se uma pessoa está acima do peso ou não
+
+$peso1 = 60;
+$peso2 = 75.5;
+$peso3 = 80.9;
+$peso4 = 100;
+
+if ($peso1 > 80) {
+    echo "Está pesado demais!!! <br>";
+} else {
+    echo "Peso dentro do limite <br>";
+}
+
+if ($peso2 > 80) {
+    echo "Está pesado demais!!! <br>";
+} else {
+    echo "Peso dentro do limite <br>";
+}
+
+if ($peso3 > 80) {
+    echo "Está pesado demais!!! <br>";
+} else {
+    echo "Peso dentro do limite <br>";
+}
+
+if ($peso4 > 80) {
+    echo "Está pesado demais!!! <br>";
+} else {
+    echo "Peso dentro do limite <br>";
+}
+?>
