1e8490d31983894cb4676993571a13c41e2a5f6b
luisfelipe98 <luisfelipemm98@gmail.com>
2023-01-27 18:52:38 -0300

    Modificando o arquivo if.php

--- Dicas/if.php
+++ Dicas/if.php
@@ -3,17 +3,17 @@
 // If é uma estrutura de controle que checa se a expressão é VERDADEIRA
 
 // Exemplos
-if (5 > 2) { // Expressão dando como VERDADEIRA, executa o que estiver dentro
+if (5 > 2) { // Expressão dada como VERDADEIRA, executa o que estiver dentro
     echo "Deu certo! Entrou dentro do if <br>";
 }
 
 if (30 % 2 > 0) { // Expressão dada como FALSA, então o if NÃO será executado
-    echo "Expressão falsa não entra no if <vr>";
+    echo "Expressão falsa não entra no if <br>";
 }
 
 // Operadores lógicos pode ser utilizados dentro do if
 if (5 <= 1 || 10 === 10) { // Com o operador lógico OR (||), só um dos lados precisa ser verdadeiro 
-    echo "Deu certo! Entrou dentro do if com condição lógica<br>";
+    echo "Deu certo! Entrou dentro do if com condição lógica <br>";
 }
 
 // Usando variáveis
@@ -21,7 +21,34 @@ $a = 15;
 $b = 5;
 $c = 1;
 
-if ($a % $b != $c ) {
-    echo "Deu certo! Entrou dentro do if de variáveis<br>";
+if ($a % $b != $c) {
+    echo "Deu certo! Entrou dentro do if de variáveis <br>";
+}
+
+// Para a exibição de uma condição FALSA denro do if, podemos adicionar o comando ELSE
+// A estrutura do ELSE é OPCIONAL
+
+// Exemplos
+if (40 != 40) {
+    echo "Deu certo! Entrou dentro do if <br>";
+} else {
+    echo "Deu errado! Entrou dentro do else <br>";
+}
+
+if (40 * 5 != 200 && "teste" === "Luis") {
+    echo "Deu certo! Entrou dentro do if com condição lógica <br>";
+} else {
+    echo "Deu errado! Entrou dentro do else com condição lógica <br>";
+}
+
+// Usando variáveis
+$a = 40;
+$b = 2;
+$c = 0;
+
+if ($a % $b != $c) {
+    echo "Deu certo! Entrou dentro do if de variáveis <br>"; 
+} else {
+    echo "Deu errado! Entrou dentro do else de variáveis <br>";
 }
 ?>
