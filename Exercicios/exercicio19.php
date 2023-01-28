3601d315d33b8e1790e1d46fddc280e855255e63
luisfelipe98 <luisfelipemm98@gmail.com>
2023-01-28 00:19:45 -0300

    Adicionando exercicio19.php

---
+++ Exercicios/exercicio19.php
@@ -0,0 +1,20 @@
+<?php
+
+// Criar um array de 10 a 100, com incremento de 10
+// Nos valores 30 e 60, pule de execução
+
+$array = [10,20,30,40,50,60,70,80,90,100];
+
+$cont = count($array) - 1;
+
+while ($cont >= 0 ) {
+    if ($array[$cont] === 60 || $array[$cont] === 30) {
+        echo "Comando Continue no número " . $array[$cont] . "<br>";
+        $cont--;
+        continue;
+    }
+    echo "Exibindo número " . $array[$cont] . "<br>";
+    $cont--;
+}
+
+?>
