d99f047bcc41e86abc7217312507a5daef7035f6
luisfelipe98 <luisfelipemm98@gmail.com>
2023-01-28 23:05:36 -0300

    Modificando include.php

--- Dicas/include.php
+++ Dicas/include.php
@@ -3,6 +3,7 @@
 // Include é um código para inserir um outro arquivo dentro de outro
 // Podemos por um arquivo .html dentro de um PHP
 // Quando o arquivo não existir, dá um Warning, mas deixa executar a página
+// NÃO ESQUECER DE POR O CAMINHO DO DIRETÓRIO DO ARQUIVO CASO TENHA
 
 // Exemplos
 
