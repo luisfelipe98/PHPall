ebf72a81a91375bccc924f07fad608918e854b39
luisfelipe98 <luisfelipemm98@gmail.com>
2023-01-28 22:58:51 -0300

    Adicionando include.php

---
+++ Dicas/include.php
@@ -0,0 +1,13 @@
+<?php
+
+// Include é um código para inserir um outro arquivo dentro de outro
+// Podemos por um arquivo .html dentro de um PHP
+// Quando o arquivo não existir, dá um Warning, mas deixa executar a página
+
+// Exemplos
+
+include "teste.php";
+include "cabecalho.html";
+include "logica.php";
+
+?>
