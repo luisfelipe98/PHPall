43930c0d8ef314d1013813d64264dbdf567d47f2
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-01 02:11:59 -0300

    Modificando funcao.php

--- Dicas/funcao.php
+++ Dicas/funcao.php
@@ -60,6 +60,21 @@ function soma3 ($n1, $n2 = -2) {
 }
 
 echo soma3(7,8) . "<br>";
-echo soma3(4) . "<br>";
+echo soma3(8) . "<br>";
+
+// Três funções interessantes de se usar
+// func_get_arg retorna o valor de um argumento da função
+// func_get_args retorna uma lista com os argumentos da função
+// func_num_args retorna a quantidade de argumentos de uma função
+
+function soma4 ($n1, $n2) {
+    echo "A função soma4 tem " . func_num_args() . " argumentos<br>";
+    echo "<pre>";
+    print_r(func_get_args());
+    echo "</pre>";
+    echo "Um dos argumentos dessa função é " . func_get_arg(1) . "<br>";
+}
+
+soma4(1,2);
 
 ?>
