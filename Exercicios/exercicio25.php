472529d2b3d8e8f04d92b773edf3ea9f5da1c300
luisfelipe98 <luisfelipemm98@gmail.com>
2023-01-31 23:40:57 -0300

    Adicionando exercicio25.php

---
+++ Exercicios/exercicio25.php
@@ -0,0 +1,13 @@
+<?php
+
+// Criar uma função que concatena strings e exiba na tela
+
+function concatenacao() {
+    $nome = "Luis";
+    $sobrenome = "Marques";
+    echo $nome . " " . $sobrenome . "<br>";
+}
+
+concatenacao();
+
+?>
