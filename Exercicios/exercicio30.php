2589cb0e2304825b52bad86ef185b6a998879abd
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-01 01:46:58 -0300

    Adicionando exercicio30.php

---
+++ Exercicios/exercicio30.php
@@ -0,0 +1,14 @@
+<?php
+
+// Criar uma função definaCorCarro
+// Valor default de vermelho
+// Exibir de todas as maneiras possíveis
+
+function definaCorCarro($cor = "vermelha") {
+    echo "A cor do carro é " . $cor . "<br>";
+}
+
+definaCorCarro();
+definaCorCarro("verde");
+
+?>
