19abd8e82fc5f1e332e3cf654a1b95ba24669f27
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-01 00:43:09 -0300

    Modificando funcao.php

--- Dicas/funcao.php
+++ Dicas/funcao.php
@@ -21,12 +21,26 @@ soma();
 echo strtoupper("testando função") . "<br>";
 
 // Parâmetros são variavéis para moldar sua execução dentro de sua função
+// PHP IGNORA PARÂMETROS DESNECESSÁRIOS
 
 function soma2($n1, $n2) {
     $soma = $n1 + $n2;
-    echo $n1 . " + " . $n2 . " = " . $soma;
+    echo $n1 . " + " . $n2 . " = " . $soma . "<br>";
 }
 
 // Chamando a função com parâmetro
-soma2(55,6) . "<br>";
+soma2(55,6);
+
+// Retorno de função
+// Objetivo é armazenar o resultado desse retorno e reutiliza-lo posteriormente
+// Utilizamos a palavra return
+
+function divisao($num1, $num2) {
+    return $num1 / $num2;
+}
+
+$n1 = 88;
+$n2 = 4;
+$divisao = divisao($n1, $n2);
+echo "O resultado da divisão entre " . $n1 . " e " . $n2 . " é " . $divisao . "<br>";
 ?>
