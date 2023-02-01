a265d4691eb8557925a6816e956d700e270deb88
luisfelipe98 <luisfelipemm98@gmail.com>
2023-01-31 23:34:09 -0300

    Adicionando exercicio24.php

---
+++ Exercicios/exercicio24.php
@@ -0,0 +1,14 @@
+<?php
+
+// Criar uma função que faz a multiplicação de números e exibir na tela
+
+function multi() {
+    $a = 2;
+    $b = 5;
+    $c = 7.5;
+    echo $a . " * " . $b . " * " . $c . " = " . $a * $b * $c;      
+}
+
+echo multi();
+
+?>
