741993048ee6f95d3a86feedb021325e3dd08b07
luisfelipe98 <luisfelipemm98@gmail.com>
2023-01-31 23:28:15 -0300

    Adicionando exercicio23.php

---
+++ Exercicios/exercicio23.php
@@ -0,0 +1,12 @@
+<?php
+
+// Criar um array de strings e utilizar a função implode e ver o resultado
+
+$array = ["Carlos", "José", "Bruna", "Mariana"];
+
+// Função implode junta um array em uma string com um seperador
+$teste = implode(",", $array);
+
+echo $teste;
+
+?>
