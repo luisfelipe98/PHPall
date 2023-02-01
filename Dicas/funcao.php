743533a1b863488f3d9c227e1241ac24a9889cfb
luisfelipe98 <luisfelipemm98@gmail.com>
2023-01-31 23:21:45 -0300

    Adicionando arquivo funcao.php

---
+++ Dicas/funcao.php
@@ -0,0 +1,23 @@
+<?php
+
+// Função são bloco de código que possue um nome
+// Realiza uma ação e é reutilizavél
+// Ajuda a reduz a duplicidade de seu código
+// Melhora a manutenção do código
+
+// Exemplos
+
+// Criação da função
+function soma() {
+    $a = 4;
+    $b = 2;
+    echo $a + $b . "<br>";
+}
+
+// Invocando minha função
+soma();
+
+// Função Própria do PHP
+echo strtoupper("testando função") . "<br>";
+
+?>
