d1248fbcb28bf08a15b96debbd65026c49c6aa1e
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-01 15:16:00 -0300

    Modificando strings.php

--- Dicas/strings.php
+++ Dicas/strings.php
@@ -71,4 +71,23 @@ echo "Interpolando variável em chaves {$teste} <br>";
 
 $texto = "Olá, meu nome é Luis <br>";
 print($texto);
+
+// Função printf
+// Semelhante ao print
+// Ela consegue imprimir valores dinâmicos através do símbolo de %
+/* Exemplos
+   %f = float
+   %d = int
+   $s = string
+   etc
+*/ 
+
+$nome = "Antonio";
+$num = 1000;
+$temperatura = 39.5;
+
+printf("Meu nome é %s <br>", $nome);
+printf("Um número é %d e o outro é %d <br>", 10, $num);
+printf("A temperatura está em %.1f ºC", $temperatura);
+
 ?>
