274d37a9b574fcceb5decef4afe6590355289a2e
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-02 00:56:33 -0300

    Modificando strings.php

--- Dicas/strings.php
+++ Dicas/strings.php
@@ -263,7 +263,7 @@ $string = implode(" ", $array);
 echo $string . "<br>";
 
 // Função Strpos
-// Serve para encontrar uma string dentro de outra string
+// Serve para encontrar A PRIMEIRA OCORRÊNCIA de uma string dentro de outra string
 // Strpos(string, o que quero encontrar)
 // O RETORNO DA FUNÇÃO DÁ O NÚMERO COMO SE FOSSE PARTE DE UM ARRAY
 // ELE ENCONTRA NÃO SÓ PALAVRAS COMPLETAS, MAS PEDAÇOS DE STRING
@@ -280,12 +280,39 @@ echo $testeEncontrar . "<br>";
 
 $testeEncontrar2 = strpos($string, "Java");
 
-// Testando para ver se deu false meu teste
+// Testando para ver se deu false
 if ($testeEncontrar2 === false) {
    echo "Palavra não encontrada <br>"; 
 }
 
 $testeEncontrar3 = strpos($string, "mo");
+
+echo $testeEncontrar3 . "<br>";
+
+// Função Strrpos
+// Serve para encontrar A ÚLTIMA OCORRÊNCIA de uma string dentro de outra string
+// Strrpos(string, o que quero encontrar)
+// O RETORNO DA FUNÇÃO DÁ O NÚMERO COMO SE FOSSE PARTE DE UM ARRAY
+// ELE ENCONTRA NÃO SÓ PALAVRAS COMPLETAS, MAS PEDAÇOS DE STRING
+// Retorna dois tipos de dados
+// Int se encontrar a palavra (será o índice de onde começa a palavra/frase)
+// Boolean se não encontrar nada
+// Exemplos
+
+$string = "Estamos testando o método strpos, com o strpos podemos encontrar strings";
+
+$testeEncontrar = strrpos($string, "strpos");
+
+echo $testeEncontrar . "<br>";
+
+$testeEncontrar2 = strrpos($string, "Java");
+
+// Testando para ver se deu false
+if ($testeEncontrar2 === false) {
+   echo "Palavra não encontrada <br>"; 
+}
+
+$testeEncontrar3 = strrpos($string, "mo");
 
 echo $testeEncontrar3 . "<br>";
 ?>
