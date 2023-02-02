1c84e79c9b06dcec7f9850c1d98154dc9a7e6c30
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-01 23:48:46 -0300

    Adicionando exercicio35.php

---
+++ Exercicios/exercicio35.php
@@ -0,0 +1,12 @@
+<?php
+
+// Resgatar a palavra queijo na frase
+// "Cadê o meu queijo? Ele estava aqui em cima"
+
+$frase = "Cadê o meu queijo? Ele estava aqui em cima";
+
+$queijo = substr($frase, 12 , 6);
+
+echo $queijo;
+
+?>
