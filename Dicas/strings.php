ee48ed1fae949b10ce4c0a594775ec24d0614747
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-02 00:26:37 -0300

    Modificando strings.php

--- Dicas/strings.php
+++ Dicas/strings.php
@@ -249,6 +249,16 @@ $stringParaArray = explode(" ", $string);
 
 echo "<pre>";
 print_r($stringParaArray);
-echo "</pre> <br>";
+echo "</pre>";
 
+// Função Implode
+// Serve para converter um array em string
+// implode(separador da string, string)
+// Exemplo
+
+$array = ["Maça", "Banana", "Laranja", "Uva", "Pera", "Abacaxi", "Coco"];
+
+$string = implode(" ", $array);
+
+echo $string . "<br>";
 ?>
