d91aff550ae968d1e68aaf29615b44ac4fea8bc0
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-01 23:52:03 -0300

    Modificando exercicio35.php

--- Exercicios/exercicio35.php
+++ Exercicios/exercicio35.php
@@ -1,12 +1,16 @@
 <?php
 
-// Resgatar a palavra queijo na frase
+// Resgatar a palavra queijo e a palavra estava na frase
 // "Cadê o meu queijo? Ele estava aqui em cima"
 
 $frase = "Cadê o meu queijo? Ele estava aqui em cima";
 
 $queijo = substr($frase, 12 , 6);
 
-echo $queijo;
+$estava = substr($frase, 24, 6);
+
+echo $queijo . "<br>";
+
+echo $estava;
 
 ?>
