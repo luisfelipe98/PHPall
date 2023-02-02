7b7ec89b5b7bd121b86426b4d53989feb027bb96
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-02 12:57:00 -0300

    Modificando strings.php

--- Dicas/strings.php
+++ Dicas/strings.php
@@ -342,7 +342,29 @@ echo $testeEncontrar3 . "<br>";
 $testeEncontrar4 = strstr($string, "Java"); // Não existe a palavra
 
 if ($testeEncontrar4 === false) {
-   echo "Texto não encontrado";
+   echo "Texto não encontrado <br>";
 }
+
+// Função Str_replace
+// Serve para substituir algo dentro de uma string
+// Str_replace(o que quer substituir, pelo o que você quer trocar, string)
+// Exemplo
+
+$string = "   Testando o str_replace para ver se funciona   ";
+
+// Quero juntar a string ser ter espaços
+$novaString = str_replace(" ", "", $string);
+
+echo "Transformando uma string assim: ";
+
+for ($i = 0; $i < strlen($string); $i++) {
+   if ($string[$i] === " ") {
+      echo "-";
+   } else {
+      echo $string[$i];
+   }
+}
+
+echo "<br> Para: " . $novaString . "<br>";
 
 ?>
