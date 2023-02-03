363011efdd793446cc8d0ee017f6d0f9b33d3091
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-03 13:06:03 -0300

    Modificando array.php

--- Dicas/array.php
+++ Dicas/array.php
@@ -162,4 +162,33 @@ echo "<pre>";
 print_r($array3);
 echo "</pre>";
 
+// Funçao Array_reduce
+// Serve para reduzir um array em um único valor
+// Array_reduce(array, função em foramto de string)
+// Exemplos
+
+// Somar todos os items de um array
+function soma ($a, $b) {
+    return $a + $b;
+}
+
+$array = range(10, 100, 7);
+
+// Usando o array_reduce
+$resultado = array_reduce($array, "soma");
+
+echo "A soma de todos os items do array é " . $resultado . "<br>";
+
+// Subtrair todos os items do array
+function subtracao ($a, $b) {
+    return $a - $b;
+}
+
+$array = range(40, -40, -13);
+
+// Usando o array_reduce
+$resultado = array_reduce($array, "subtracao");
+
+echo "A subtracao de todos os items do array é " . $resultado . "<br>";
+
 ?>
