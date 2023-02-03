2a78cabf196a6c0c749d715ab3aa258437f1a9cc
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-03 17:15:45 -0300

    Modificando array.php

--- Dicas/array.php
+++ Dicas/array.php
@@ -330,4 +330,26 @@ $array = [-4, 7, 45.1, 35, -19, 25];
 
 echo "A soma dos números do array é " . array_sum($array) . "<br>";
 
+// Função Array_merge
+// Serve para unir dois ou mais arrays
+// Array_merge(array)
+// Exemplo
+
+$array1 = [4, 9, 15];
+$array2 = [-1, -15, -67.6];
+$array3 = [-44.6, -90, -7.2];
+
+// Juntando os arrays
+$arrayMerge = array_merge($array1, $array2, $array3);
+
+// Exibindo na tela
+echo "O array depois do merge ficou ";
+for ($k = 0; $k < count($arrayMerge); $k++) {
+    if (count($arrayMerge) - 1 === $k) { // Último elemento
+        echo $arrayMerge[$k] . ". <br>";
+    } else {
+        echo $arrayMerge[$k] . ", ";
+    }
+}
+
 ?>
