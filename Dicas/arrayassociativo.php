91c3d381eb25dad3e6709e98aa569a3297bdaf17
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-03 00:01:50 -0300

    Modificando arrayassociativo.php

--- Dicas/arrayassociativo.php
+++ Dicas/arrayassociativo.php
@@ -56,4 +56,39 @@ echo "<pre>";
 print_r($valores);
 echo "</pre>";
 
+// Função Array_key_exists
+// Serve para descobrir se dentro do array existe a chave especificada
+// Array_key_exists(qual é chave, array)
+// Exemplo
+
+$pessoa = [
+    "nome" => "Luis",
+    "idade" => 24
+];
+
+// Checando se existe a chave "nome" no array
+if (Array_key_exists("nome", $pessoa)) { 
+    echo "A chave existe <br>";
+} else {
+    echo "A chave não existe <br>";
+}
+
+// Checando se existe a chave "profissao" no array
+if (Array_key_exists("profissao", $pessoa)) { 
+    echo "A chave existe <br>";
+} else {
+    echo "A chave não existe <br>";
+}
+
+// Função Isset
+// Serve para descobrir se existe algo
+// isset (o que quer descobrir)
+// Retorna um boolean
+
+if (isset($pessoa["idade"])) {
+    echo "A chave existe <br>";
+} else {
+    echo "A chave não existe <br>";
+}
+
 ?>
