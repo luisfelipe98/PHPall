4bfde42b660e7bfd95ea242c4440ec39397e2f61
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-03 15:56:10 -0300

    Modificando array.php

--- Dicas/array.php
+++ Dicas/array.php
@@ -320,4 +320,14 @@ for ($j = 0; $j < count($arrayAleatorio); $j++) {
     }
 }
 
+// Função Array_sum
+// Serve para somar todos os elementos do array
+// A função IGNORA OUTROS TIPOS DE VALORES QUE NÃO FOREM NÚMEROS
+// Array_sum(array)
+// Exemplos
+
+$array = [-4, 7, 45.1, 35, -19, 25];
+
+echo "A soma dos números do array é " . array_sum($array) . "<br>";
+
 ?>
