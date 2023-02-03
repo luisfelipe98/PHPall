2410a4584271f1918145b48ac46affd0ecba85cc
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-03 12:01:50 -0300

    Modificando arrayassociativo.php

--- Dicas/arrayassociativo.php
+++ Dicas/arrayassociativo.php
@@ -131,4 +131,22 @@ extract($pessoa);
 
 echo "A variável nome DEPOIS do extract : " . $nome . "<br>";
 
+// Função Compact
+// Serve para criar um array associativo de acordo com várias variáveis
+// É o efeito ao contrário da função extract
+// Compact(nomes das chaves em string)
+// Exemplo
+
+$marca = "BMW";
+$motor = "3.0";
+$portas = 4;
+$cambio = "automático";
+$temTetoSolar = true;
+
+$carro = compact("marca", "motor", "cambio", "portas", "temTetoSolar"); // Criando o array e informando as chaves do mesmo
+
+echo "<pre>";
+print_r($carro);
+echo "</pre>";
+
 ?>
