ccbaf498b4452e1534455d930156612e2e48908d
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-06 00:22:21 -0300

    Modificando loops.php

--- Linguagem/loops.php
+++ Linguagem/loops.php
@@ -2,6 +2,7 @@
 
 // Loops são estruturas de repetição
 // Muito utilizados em arrays
+// São destes tipo: For, Foreach, Do While, While e Loops Alinhados
 // Comandos break e/ou continue são utilizados neles
 // Comando break serve para sair de um loop
 // Comando Continue serve para pular uma execução do loop
@@ -31,91 +32,4 @@ echo "<br>";
 echo "Saiu do loop com Break e Continue";
 echo "<br><br><br>";
 
-// While
-
-echo "Loop com o While <br> <br>";
-
-$cont = 1;
-
-while ($cont != 11) {
-    if ($cont === 1) {
-        echo "Passando pelo loop " . $cont ." vez <br>";
-        $cont++;
-    } 
-    
-    echo "Passando pelo loop " . $cont ." vezes <br>";
-    $cont++;
-}
-echo "<br>";
-echo "Saiu do loop do while";
-echo "<br><br><br>";
-
-// Do While
-
-echo "Loop com o Do While <br> <br>";
-
-$cont = 1;
-
-do {
-    if ($cont === 1) {
-        echo "Passando pelo loop " . $cont ." vez <br>";
-        $cont++;
-    } 
-    
-    echo "Passando pelo loop " . $cont ." vezes <br>";
-    $cont++;
-
-} while ($cont <= 10);
-
-echo "<br>";
-echo "Saiu do loop do do while";
-echo "<br><br><br>";
-
-// For
-
-echo "Loop com For <br> <br>";
-
-for ($i = 1; $i <=10; $i++) {
-    if ($i === 1) {
-        echo "Passando pelo loop " . $i . " vez <br>";
-    } else {
-        echo "Passando pelo loop " . $i . " vezes <br>";
-    }
-}
-
-echo "<br>";
-echo "Saiu do loop do for";
-echo "<br><br><br>";
-
-// Loop Alinhado
-// São loop dentro de outro loop
-// É bom cada um ter seu contador
-// Cuidado com os loops infinitos
-
-echo "Loop Alinhado <br> <br>";
-$cont1 = 1; // 1º contador
-$qtdeExe = 1; // Contador para descobrir quantas execuções de todos os loops
-while ($cont1 <= 10) {
-    if ($cont1 === 1) {
-       echo $qtdeExe . " - Passando pelo loop externo " . $cont1 . " vez <br>"; 
-    } else {
-       echo $qtdeExe . " - Passando pelo loop externo " . $cont1 . " vezes <br>";
-    }
-    
-    $cont2 = 1; // Segundo contador
-        while ($cont2 <= 5) {
-            $qtdeExe++;
-            if ($cont2 === 1) {
-                echo $qtdeExe . " - Passando pelo loop interno " . $cont2 . " vez <br>";
-                $cont2++;
-            } else {
-                echo $qtdeExe . " - Passando pelo loop interno " . $cont2 . " vezes <br>";
-                $cont2++;
-            }
-        }
-    $cont1++;
-    $qtdeExe++;
-    echo "<br>";
-}
-echo "Saiu do loop alinhado <br>";
 ?>
