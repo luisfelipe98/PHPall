f53ac622a10c896526594dc6b40cfbbda8899a2e
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-06 01:38:16 -0300

    Adicionando escape.php

---
+++ Linguagem/Conceitos/Variáveis/Strings/escape.php
@@ -0,0 +1,20 @@
+<?php
+
+// Valores de escape
+// É obrigatório utilizar "" (aspas duplas)
+/* Exemplos
+   \n = nova linha
+   \t = tab
+   \\ = barra invertida
+   \$ = símbolo do dolar
+   etc
+*/ 
+
+// É bem utilizado em sistemas que respondem com o PHP
+// É utilizado em sistemas de texto puro, não tem necessidade se for na Web
+
+// Função print
+// Semelhante ao echo
+// Ela imprime uma string que foi passada como argumento
+
+?>
