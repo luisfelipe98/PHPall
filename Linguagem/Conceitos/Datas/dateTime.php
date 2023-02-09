1db7d62b0e86603928d8b3661dfe1bee86e8b386
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-08 23:53:58 -0300

    Adicionando dateTime.php

---
+++ Linguagem/Conceitos/Datas/dateTime.php
@@ -0,0 +1,14 @@
+<?php
+
+// Objeto DateTime
+// Permite tratar a data como um objeto
+// Se não passar parâmetro, considera a data ATUAL
+// Exemplos
+
+$data = new DateTime();
+
+echo "<pre>";
+print_r($data);
+echo "</pre>";
+
+?>
