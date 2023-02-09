796a608e420a0d088e7d95bace450d261d231d1d
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-09 00:07:34 -0300

    Modificando dateTime.php

--- Linguagem/Conceitos/Datas/dateTime.php
+++ Linguagem/Conceitos/Datas/dateTime.php
@@ -11,4 +11,33 @@ echo "<pre>";
 print_r($data);
 echo "</pre>";
 
+// Métodos Format e Modify
+// Format formata a data
+// Modify altera a data
+// Exemplos
+
+$data = new DateTime();
+
+// Data Atual
+// Usando o format
+echo $data->format("d/m/y") . "<br>";
+
+echo $data->format("D - M - Y") . "<br>";
+
+echo $data->format("l / F / Y") . "<br>";
+
+// Usando o modify
+// Data + 5 dias
+$data->modify("+ 5 days"); 
+
+echo $data->format("d/m/y") . "<br>";
+
+$data->modify("+ 2 months");
+
+echo $data->format("d/m/y") . "<br>";
+
+$data->modify("- 3 years");
+
+echo $data->format("d/m/y") . "<br>";
+
 ?>
