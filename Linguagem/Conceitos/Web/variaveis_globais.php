7aad1591357c1fbc11577168f1f5a6da4ced27d7
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-09 14:24:04 -0300

    Adicionando variaveis_globais.php

---
+++ Linguagem/Conceitos/Web/variaveis_globais.php
@@ -0,0 +1,12 @@
+<?php
+
+// Para lidar com requisições HTTP, o PHP apresenta variáveis globais
+// Exemplos
+// $_ENV = variáveis de ambiente
+// $_GET = parâmetros que foram enviados por requisição GET 
+// $_POST = parâmetros que foram enviados por requisição POST
+// $_COOKIE = valores de cookies
+// $_SERVER = informações sobre o servidor
+// $_FILES = informações sobre os arquivos que vieram por upload
+
+?>
