20085f65a75e12f455d7bc6f5496baa0206cc4e6
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-09 00:16:11 -0300

    Modificando dateTime.php

--- Linguagem/Conceitos/Datas/dateTime.php
+++ Linguagem/Conceitos/Datas/dateTime.php
@@ -40,4 +40,31 @@ $data->modify("- 3 years");
 
 echo $data->format("d/m/y") . "<br>";
 
+// Métodos setDate e setTime
+// SetDate recebe ano, mês e dia, alterando a data completamente
+// SetTime recebe hora, minuto e segundo, alterando o tempo completamente
+// Exemplos
+
+$data = new DateTime();
+
+echo "<pre>";
+print_r($data);
+echo "</pre>";
+
+// Usando o setDate
+$data->setDate(1999, 12, 01);
+
+echo "<pre>";
+print_r($data);
+echo "</pre>";
+
+// Usando o setTime
+$data->setTime(05, 12, 10);
+
+echo "<pre>";
+print_r($data);
+echo "</pre>";
+
+echo $data->format("d/m/y") . "<br>";
+
 ?>
