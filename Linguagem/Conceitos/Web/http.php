8bf9542fae3784ef0918dba8a790a5f1dcae9283
luisfelipe98 <luisfelipemm98@gmail.com>
2023-02-09 14:15:11 -0300

    Adicionando http.php

---
+++ Linguagem/Conceitos/Web/http.php
@@ -0,0 +1,19 @@
+<?php
+
+// A Internet utiliza o protocolo HTTP
+// HTTP significa HuperText Transfer Protocol
+// Esse protocolo é o que o PHP utiliza para se comunicar com o cliente e a máquina do servidor
+// Quando entramos em um site, fazemos uma requisição e a resposta será o que você mandou na requisição
+// Exemplo: Fazemos uma requisição pedindo a exibição do site do Google, a resposta é o site do Google
+
+// Métodos HTTP
+// Quando fazemos uma requisição, enviamos métodos junto com elas
+// Alguns deles são:
+// GET solicita a apresentação de um recurso. Ex.: visualizar uma página WEB
+// POST faz o envio de dados ao servidor. Ex.: Página de cadastro de um usuário
+// PUT serve para atualização de dados
+// DELETE serve para a remoção de dados
+// PATCH é a atualização de dado específico
+// GET e POST são os mais utilizados
+
+?>
